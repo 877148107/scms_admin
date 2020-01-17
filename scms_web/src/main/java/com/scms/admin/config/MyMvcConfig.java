a24ce22f6637482913f007e8a9a632d0ca4492de
william.wangmy <877148107@qq.com>
2020-01-17 11:36:14 +0800

    2020.1.17 初始化提交

---
+++ scms_web/src/main/java/com/scms/admin/config/MyMvcConfig.java
@@ -0,0 +1,30 @@
+package com.scms.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ClassName: MyMvcConfig
+ * =================================================
+ * @Description: SpringMvc的扩展配置类，相当于原来的xml配置
+ * =================================================
+ * CreateInfo:
+ * @Author: William.Wangmy
+ * @Email: wangmingyong2018@163.com
+ * @CreateDate: 2020/1/17 10:54
+ * @Version: V1.0
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加URL映射跳转页面
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+    }
+}
